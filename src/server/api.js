122e5aff51f7b3eaac0c1ca22bf5863868d3dcb2
Djamel Bousfira <djamel@tomanage.fr>
2018-01-16 17:29:11 +0100

    POST

---
+++ src/server/api.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const config = require('./config.js');
+const uuidv4 = require('uuid/v4');
+
+// define the home page route
+router.get('/', function(req, res) {
+  res.send('GET');
+});
+
+// OK
+router.post('/', function(req, res) {
+  req.query.id = uuidv4();
+  fs.writeFile(config.data + '/' + req.query.id + '.json', JSON.stringify(req.query), (err) => {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  });
+
+  res.send(req.query);
+});
+
+router.get('/{id}', function(req, res) {
+  fs.readFile('/../../data/episodes.json', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+  res.send('GET all');
+});
+
+router.delete('/{id}', function(req, res) {
+  res.send('DELETE');
+});
+
+router.put('/{id}', function(req, res) {
+  res.send('PUT');
+});
+
+module.exports = router;
--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const api = require('./api.js');
 const config = require('./config.js');
+var app = express();
+
+app.use('/api/episodes', api);
+app.listen(config.port);
